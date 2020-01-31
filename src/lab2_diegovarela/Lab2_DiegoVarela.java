cf289b7970d09c39fccfbda9bd7c47411d9bbd65
Diego Varela <diegocrash124@gmail.com>
2020-01-31 15:43:33 -0600

    Primer commit

---
+++ src/lab2_diegovarela/Lab2_DiegoVarela.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_diegovarela;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author diego
+ */
+public class Lab2_DiegoVarela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        ArrayList<Personaje> lista = new ArrayList();
+        lista.add(new Personaje("Barbaro","Claus","Enano","90cm","El no reparte regalos","Brinstar","60lbs","40"));
+        char resp = 's';
+        while (resp == 's' || resp == 'S') {
+            System.out.println("Menu");
+            System.out.println("1) Creacion de personaje");
+            System.out.println("2) Modificar de personajes");
+            System.out.println("3) Ver atribuutos de personaje");
+            System.out.println("4) Eliminar Personajes");
+            System.out.println("5) Combate");
+            System.out.println("6) Salida");
+            int opcion = entrada.nextInt();
+            switch (opcion) {
+                //Opcion de crear personaje
+                case 1:
+                    System.out.println("Ingrese la clase personaje que desea ser(Mago,Barbaro,Clerigo O Picaro)");
+                    String clase = entrada.next();
+                    System.out.println("Ingrese el nombre del personaje sin espacios");
+                    String nombre = entrada.next();
+                    System.out.println("Ingrese la raza(Mediano, Enano, Elfo y Humano)");
+                    String raza = entrada.next();
+                    System.out.println("Ingrese la descripcion del personaje");
+                    entrada=new Scanner(System.in);
+                    String des = entrada.nextLine();
+                    des = entrada.nextLine();
+                    System.out.println("Ingrese la estatura");
+                    String estatura = entrada.next();
+                    System.out.println("Ingrese el peso");
+                    String peso = entrada.next();
+                    System.out.println("Ingrese la nacionalidad(Norfair ,Brinstar , Maridia,Zebes ,Crateria)");
+                    String nac = entrada.next();
+                    System.out.println("Ingrese la edad");
+                    String años = entrada.next();
+                    lista.add(new Personaje(clase, nombre, raza, estatura, des, nac, peso, años));
+
+                    break;
+                //Opcion de modificar
+                case 2:
+                    for (int i = 0; i < lista.size(); i++) {
+                        System.out.println(((Personaje)lista.get(i)));
+                    }
+                    System.out.println("Que posicion desea modificar");
+                    int pos =entrada.nextInt();
+                    //menu de modificaciones
+                    System.out.println("¿Que desea modificar?");
+                    System.out.println("1) Clase");
+                    System.out.println("2) Raza");
+                    System.out.println("3) Nombre");
+                    System.out.println("4) Nacionalidad");
+                    System.out.println("5) Años");
+                    System.out.println("6) Peso");
+                    System.out.println("7) Descripcion");
+                    System.out.println("8) Estatura");
+                    int opcion2 = entrada.nextInt();
+                    switch (opcion2) {
+                        case 1:
+                            System.out.println("Escriba ahora");
+                            String cl=entrada.next();
+                            ((Personaje)lista.get(pos)).setclase(cl);
+                            break;
+                        case 2:
+                            System.out.println("Escriba ahora");
+                            String rac=entrada.next();
+                            ((Personaje)lista.get(pos)).setraza(rac);
+                            break;
+                        case 3:
+                            System.out.println("Escriba ahora");
+                            String no=entrada.next();
+                            ((Personaje)lista.get(pos)).setnombre(no);
+                            break;
+                        case 4:
+                            System.out.println("Escriba ahora");
+                            String naci=entrada.next();
+                            ((Personaje)lista.get(pos)).setnacionalidad(naci);
+                            break;
+                        case 5:
+                            System.out.println("Escriba ahora");
+                            String añ=entrada.next();
+                            ((Personaje)lista.get(pos)).setaños(añ);
+                            break;
+                        case 6:
+                            System.out.println("Escriba ahora");
+                            String pes=entrada.next();
+                            ((Personaje)lista.get(pos)).setpeso(pes);
+                            break;
+                        case 7:
+                            System.out.println("Escriba ahora");
+                            String esta=entrada.next();
+                            ((Personaje)lista.get(pos)).setestatura(esta);
+                            break;
+                        case 8:
+                            System.out.println("Escriba ahora");
+                            String desi=entrada.next();
+                            ((Personaje)lista.get(pos)).setdescripcion(desi);
+                            break;
+                    }
+                    break;
+                case 3:
+                    //ver atributos de personaje
+                    for (int i = 0; i < lista.size(); i++) {
+                        System.out.println(lista.get(i));
+                    }
+                    System.out.println("¿De que jugador desea ver los atributos?");
+                    int pos2=entrada.nextInt();
+                    System.out.println("Raza:"+((Personaje)lista.get(pos2)).getraza());
+                    System.out.println("Nombre:"+((Personaje)lista.get(pos2)).getnombre());
+                    System.out.println("Nacionalidad:"+((Personaje)lista.get(pos2)).getnacionalidad());
+                    System.out.println("Estatura:"+((Personaje)lista.get(pos2)).getestatura());
+                    System.out.println("Peso:"+((Personaje)lista.get(pos2)).getpeso());
+                    System.out.println("Años:"+((Personaje)lista.get(pos2)).getaños());
+                    System.out.println("Descripcion:"+((Personaje)lista.get(pos2)).getdescripcion());
+                    break;
+                case 4:
+                    //eliminar personaje
+                    for (int i = 0; i < lista.size(); i++) {
+                        System.out.println(lista.get(i));
+                    }
+                    System.out.println("¿Que personaje desea borrar?");
+                    int num=entrada.nextInt();
+                    lista.remove(num);
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+
+            }
+        }
+    }
+
+
+}
---
+++ src/lab2_diegovarela/Personaje.java
@@ -0,0 +1,196 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_diegovarela;
+
+import java.util.Random;
+
+/**
+ *
+ * @author diego
+ */
+public class Personaje {
+
+    Random random = new Random();
+    private String clase;
+    private String nombre;
+    private String raza;
+    private String estatura;
+    private String descripcion;
+    private String nacionalidad;
+    private String años;
+    private String peso;
+    private int hp;
+    private int dg;
+    private int cs;
+    private int ac;
+
+    public Personaje() {
+
+    }
+
+    public Personaje(String c, String n, String r, String e, String d, String nac,String p,String an) {
+        clase = c;
+        nombre = n;
+        if (r == "Mediano" || r == "Enano" || r == "Humano" || r == "Elfo") {
+            raza = r;
+        }
+        estatura = e;
+        descripcion = d;
+        if (nac == "Norfair" || nac == "Brinstar" || nac == "Maridia" || nac == "Zebes" || nac == "Criteria") {
+            nacionalidad = nac;
+        }
+        peso=p;
+        años=an;
+        setHp();
+        setAc();
+        setDg();
+        setCs();
+
+    }
+
+    public String getnombre() {
+        return nombre;
+    }
+
+    public void setnombre(String n) {
+        this.nombre = n;
+    }
+
+    public String getclase() {
+        return clase;
+    }
+
+    public void setclase(String clas) {
+        this.clase = clas;
+    }
+
+    public String getraza() {
+        return raza;
+    }
+
+    public void setraza(String raz) {
+        this.raza = raz;
+    }
+
+    public String getestatura() {
+        return estatura;
+    }
+
+    public void setestatura(String est) {
+        this.estatura = est;
+    }
+
+    public String getdescripcion() {
+        return descripcion;
+    }
+
+    public void setdescripcion(String des) {
+        this.descripcion = des;
+    }
+
+    public String getnacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setnacionalidad(String nac) {
+        this.nacionalidad = nac;
+    }
+     public String getpeso() {
+        return peso;
+    }
+
+    public void setpeso(String pe) {
+        this.peso = pe;
+    }
+     public String getaños() {
+        return años;
+    }
+
+    public void setaños(String ano) {
+        this.años = ano;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp() {
+        if (raza == "Enano") {
+            hp = 80 + random.nextInt(100);
+        }
+        if (raza == "Humano") {
+            hp = 40 + random.nextInt(75);
+        }
+        if (raza == "Mediano") {
+            hp = 50 + random.nextInt(60);
+        }
+        if (raza == "Elfo") {
+            hp = 50 + random.nextInt(70);
+        }
+    }
+
+    public int getCs() {
+        return cs;
+    }
+
+    public void setCs() {
+        if (clase == "Barbaro") {
+            cs = 97;
+        }
+        if (clase == "Clerigo") {
+            cs = 97;
+        }
+        if (clase == "Mago") {
+            cs = 101;
+        }
+        if (clase == "Picaro") {
+            cs = 80;
+        }
+    }
+
+    public int getDg() {
+        return dg;
+    }
+
+    public void setDg() {
+        if (clase == "Barbaro") {
+            dg = 15 + random.nextInt(30);
+        }
+        if (clase == "Clerigo") {
+            dg = 5 + random.nextInt(15);
+        }
+        if (clase == "Mago") {
+            dg = 5 + random.nextInt(10);
+        }
+        if (clase == "Picaro") {
+            dg = 15 + random.nextInt(25);
+        }
+    }
+
+    public int getAc() {
+        return ac;
+    }
+
+    public void setAc() {
+        if (clase == "Barbaro") {
+            ac = 65;
+        }
+        if (clase == "Clerigo") {
+            ac = 40;
+        }
+        if (clase == "Mago") {
+            ac = 20;
+        }
+        if (clase == "Picaro") {
+            ac = 50;
+        }
+    }
+    @Override
+     public String toString() {
+        return nombre;
+    }
+
+}
